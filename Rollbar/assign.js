a1a182cba0fb459cb6f31d044bdb3223a5e0162b
Tanya1509062 <tanyavimal0011@gmail.com>
2025-02-04 12:34:58 +0530

    Add files via upload

---
+++ Rollbar/assign.js
@@ -0,0 +1,9 @@
+
+        const fruits = ["Banana", "Orange", "Apple", "Mango"];
+        let fruit = fruits[2];
+        
+        document.getElementById("demo").innerHTML = fruit;
+
+        console.log("Hello World");
+        
+ 
